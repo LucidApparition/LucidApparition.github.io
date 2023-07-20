12d3c2b24755cb4b433d61d6c87aa609fec6fb32
Jeff <jagrafe94@gmail.com>
2023-07-20 16:17:13 -0400

    3d_portfolio

---
+++ src/components/Contact.jsx
@@ -0,0 +1,56 @@
+import React, { useState, useRef } from 'react';
+import { motion } from 'framer-motion';
+import emailjs from '@emailjs/browser';
+import { styles } from '../styles';
+import { EarthCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
+import { slideIn } from '../utils/motion';
+
+const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState ({
+    name: '',
+    email: '',
+    message: '',
+  });
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {}
+  const handleSubmit = (e) => {}
+  return (
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn('left', "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+        <p className={styles.sectionSubText}>
+          Get in touch
+        </p>
+        <h3 className={styles.sectionHeadText}>
+          Contact.
+        </h3>
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">
+              Your Name
+            </span>
+            <input 
+              type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's you name?"
+              className="bg-tertiary py-4 px06 placeholder: text-secondary 
+                text-white round-lg outlined-none"
+            />
+          </label>
+        </form>
+      </motion.div>
+    </div>
+  )
+}
+
+export default SectionWrapper(Contact, "contact")
