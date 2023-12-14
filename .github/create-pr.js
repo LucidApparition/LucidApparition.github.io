eaa21f858c534ae23fb16b365d0f2d9549919361
Jeff Grafe <jagrafe94@gmail.com>
2023-12-13 23:53:36 -0500

    update Octokit with debugging

--- .github/create-pr.js
+++ .github/create-pr.js
@@ -5,22 +5,25 @@ import { execSync } from 'child_process';
 // Custom fetch implementation using node-fetch
 import fetch from 'node-fetch';
 
+// For debugging
+console.log('Environment variables:', process.env);
+
+// Fix: Set GITHUB_REF manually for local testing
+process.env.GITHUB_REF = process.env.GITHUB_REF || 'refs/heads/development'; // Replace 'development' with the default branch name
+
+const branch = process.env.GITHUB_REF.replace('refs/heads/', '');
+console.log('Branch:', branch);
+
 const octokit = new Octokit({
   auth: process.env.GH_TOKEN,
   request: { fetch },
 });
 
 // Get repository owner and name from Git configuration
-const repoOwner = execSync('git config --get remote.origin.url')
-  .toString()
-  .match(/github\.com[:/](.*?)\.git/)[1]
-  .split('/')[0];
-const repoName = execSync('git config --get remote.origin.url')
-  .toString()
-  .match(/github\.com[:/](.*?)\.git/)[1]
-  .split('/')[1];
+const repoUrl = execSync('git config --get remote.origin.url').toString().trim();
+const repoOwner = repoUrl.match(/github\.com[:/](.*?)\.git/)[1].split('/')[0];
+const repoName = repoUrl.match(/github\.com[:/](.*?)\.git/)[1].split('/')[1];
 
-const branch = process.env.GITHUB_REF.replace('refs/heads/', '');
 const prTitle = `Auto PR: ${branch}`;
 const prTemplatePath = '.github/prTemplate.md';
 const prBody = `Auto-generated pull request after push to ${branch}\n\n${fs.readFileSync(prTemplatePath, 'utf-8')}`;
@@ -32,4 +35,8 @@ octokit.pulls.create({
   head: branch,
   base: 'master',
   body: prBody,
+}).then(response => {
+  console.log('Pull request created successfully:', response.data);
+}).catch(error => {
+  console.error('Error creating pull request:', error);
 });
